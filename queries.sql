09ab25803a6039eb03e03c39e06f9b64b02d92d0
jaunceymyles <jaunceymyles@gmail.com>
2025-06-09 14:53:13 +0100

    Added queries and some stakeholder questions.
    Changed query 3 and added rough answer for query 4 (need clarification) as well as adding query 6 and 7.
    
    Also added some deliverable questions to ask

--- queries.sql
+++ queries.sql
@@ -14,15 +14,54 @@ FROM
 SELECT
 	COUNT(*)
 FROM
-	mystic_manuscript.right_join.columns;
+	mystic_manuscript.columns
+WHERE
+	table_name = 'right_join';
 
 -- query4
-
+SELECT
+	SUM(qtr) +
+	SUM('year') +
+	SUM('month') +
+	SUM(date_day) +
+	SUM(special_day) +
+	SUM(online_sales_offers) +
+	SUM('day') +
+	SUM(weekend) +
+	SUM(morning) +
+	SUM(afternoon) +
+	SUM(evening) +
+	SUM(night) +
+	SUM(gender_num) +
+	SUM(gender) +
+	SUM(quantity) +
+	SUM(item_price) +
+	SUM(shipping_price) +
+	SUM(total_amount) +
+	SUM(profit_percentage) +
+	SUM(profit_inr) +
+	SUM(cost_price) AS total
+FROM
+	mystic_manuscript.right_join;
+--SUM of all numeric columns
+--Need to ask what columns to be summed
 
 -- query5
 
+--SUM OF ROW SUMS?
 
 -- query6
 
+SELECT
+	*
+FROM
+	mystic_manuscript.right_join r
+WHERE
+	r.id IS IN ( /* 5 Random Rows*/ )
 
 -- query7
+	
+SELECT
+	*
+FROM
+	mystic_manuscript.right_join;

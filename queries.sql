58162d2865fdcfe851c3246556003b988871347f
jaunceymyles <jaunceymyles@gmail.com>
2025-06-09 14:02:18 +0100

    Update queries.sql
    Added queries 1 to 3

--- queries.sql
+++ queries.sql
@@ -1,11 +1,20 @@
 -- query1
-
+SELECT
+	COUNT(*)
+FROM
+	mystic_manuscript.right_join;
 
 -- query2
-
+SELECT
+	COUNT(DISTINCT *)
+FROM
+	mystic_manuscript.right_join;
 
 -- query3
-
+SELECT
+	COUNT(*)
+FROM
+	mystic_manuscript.right_join.columns;
 
 -- query4
 
